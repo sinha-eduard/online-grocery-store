174b24e6c5a1b7399adf4d12d9b769dbebc17942
sinha-eduard <77357095+sinha-eduard@users.noreply.github.com>
2024-09-24 17:16:18 -0400

    Reorganized

--- index.js
+++ index.js
@@ -15,12 +15,22 @@ mongoose.connect('mongodb://localhost:27017/groceryStore')
     });
 
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "public/views"));
 app.set("view engine", "ejs");
 
 app.get("/home", async (req, res) =>{
-    const products = await Product.find({})
-    res.render('products/home', {products})
+    const {category} = req.query;
+    if(category){
+        if(category == "fruit" || category == "vegetable" ||category == "baked" || category == "dairy"){
+            const products = await Product.find({category : category})
+            res.render("category", { products, category })
+        } else {
+            res.redirect("/404")
+        }
+    } else{
+        const products = await Product.find({})
+    res.render('home', {products})
+    }
 })
 
 app.get("/", async (req, res) =>{
@@ -32,12 +42,12 @@ app.get("/products", async (req, res) =>{
     if(category){
         if(category == "fruit" || category == "vegetable" ||category == "baked" || category == "dairy"){
             const products = await Product.find({category : category})
-            res.render("products/category", { products, category })
+            res.render("category", { products, category })
         } else {
             res.redirect("/404")
         }
     } else{
-        res.redirect("/home")
+        res.redirect("home")
     }
 })
 
@@ -45,7 +55,7 @@ app.get("/products/:id", async (req, res) => {
     const {id} = req.params;
     try{
         const product = await Product.findById(id)
-        res.render("products/product", { product })
+        res.render("product", { product })
     }catch(e){
         res.redirect("/404")
     }
